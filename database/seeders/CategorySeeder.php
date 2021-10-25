b037e315a2e8b36967e134a3372e916631f65419
Csongor Szegleti <csongor.szegleti@protonmail.com>
2021-10-25 18:43:45 +0200

    Create categoryies table seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,14 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::create(['name' => 'Movie (HUN)']);
+        Category::create(['name' => 'Movie (ENG)']);
+        Category::create(['name' => 'Series (HUN)']);
+        Category::create(['name' => 'Series (ENG)']);
+        Category::create(['name' => 'Music']);
+        Category::create(['name' => 'Game']);
+        Category::create(['name' => 'Program']);
+        Category::create(['name' => 'E-book (HUN)']);
+        Category::create(['name' => 'E-book (ENG)']);
     }
 }
